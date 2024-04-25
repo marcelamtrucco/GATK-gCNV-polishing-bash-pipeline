76cbb9f83925d591cc2284d7f876de6556b87a8e
marcelamtrucco <135526342+marcelamtrucco@users.noreply.github.com>
2024-04-24 23:57:09 -0300

    Add files via upload

---
+++ gatk_cnv-filter.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# This script has been developed at the Institute of Biological Chemistry of the School of Exact and Natural Science (IQUIBICEN)
+# of University of Buenos Aires.
+#____________________________________________________________________________________________________________________________
+# After gGATK-CNV analysis with gatk_case.sh, this pipeline is used for sample and variant filtering
+
+#1) Sample filtering: remove samples with more than 100 CNVs.
+#2) Variant filtering: remove (common) CNVs with frequency > 1%. 
+
+#Paths to Directories required for this pipeline:
+
+output_dir=${HOME}/output_dir
+case=${output_dir}/case
+case-results=${case}/case-results
+newfiles= ${output_dir}/newfiles
+unique_cnvs=${newfiles}/unique_cnvs
+
+# gGATK-CNV output are VCF segment files (zcat genotyped-segments-case-sample_0.vcf), one per sample, 
+# saved in case-results directory. The fiel is edited as follows,generating _type.bed files in the newfiles folder.
+
+if [ ! $(ls -A ${newfiles} ];then
+	for file in ${case-results}/*;do
+		cd $file;
+		zcat genotyped-segments-case-sample_0.vcf| \
+		grep -v "#"| \
+		cut -f1,2,5,6,8,10| \
+		sed 's/END=//g'|sed 's/:/\t/g'| \
+		awk '{if ($3!=".") print $1"\t"$2"\t"$5"\t"$3"\t"$4"\t"$6"\t"$7"\t"$8"\t"}'> \
+		${newfiles}/(basename $file)_type.bed;
+		cd ../;
+done
+else
+	echo "Case VCF segment files were processed to access CNV calls"
+fi
+# 1) Sample filtering: Samples with more than 100 CNVs are removed from _type.bed files
+
+for file in ${newfiles}/*_type.bed;do
+	count=$(cat $file|wc -l);
+	if [ $count -gt 100 ];
+	then \
+		rm $file;
+	fi;
+done
+
+# 2) Variant filtering:
+# this script concatenate the fields: chr/start/end/cnv_type in intervals with '@', then identical concatenated intervals 
+# are sorted and counted. Counts greater than 3 are removed,keeping unfrequent CNVs,saved as unique_cnvs.bed
+
+if [ ! -f  ${newfiles}/unique_cnvs.bed ];then
+	for file in ${newfiles}/*_type.bed;do
+       		cat $file| \
+        	awk '{print $1,$2,$3,$4}'| \
+        	sed 's/ /@/g';
+	done|sort|uniq -c|sort -nrk1,1|awk -F " " '{if ($1 <= 2) print $2}'|sed 's/@/\t/g'|sed 's/^chr//g'| \
+	sort -nk1,1 -nk2,2|sed 's/^/chr/g' > ${newfiles}/unique_cnvs.bed 
+fi
+
+# Search and filter samples for unfrequent CNVs, listed in unique_cnvs.bed:
+
+if [ ! $(ls -A ${unique_cnvs}/*) ];then
+	for file in ${newfiles}/*_type.bed;do
+		 awk '(FNR==NR){a[$1,$2,$3,$4];next} (($1,$2,$3,$4) in a) {print $0}' ${newfiles}/unique_cnvs.bed $file \
+		 > ${unique_cnvs}/$(basename $file _type.bed).bed;
+	done 
+else
+	echo "Samples Common CNVs filtering already done "
+fi
+
---
+++ gatk_segment_extension.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This script has been developed at the Institute of Biological Chemistry of the School of Exact and Natural Science (IQUIBICEN)
+# of University of Buenos Aires.
+#____________________________________________________________________________________________________________________________
+# After Common CNVs filtering, using cnv-filter.sh, each CNV interval is extended by 50% length at each side
+
+#Paths to Directories required for this pipeline:
+
+output_dir=${HOME}/output_dir
+newfiles= ${output_dir}/newfiles
+unique_cnvs=${newfiles}/unique_cnvs
+extended_segments=${unique_cnvs}/extended_segments
+mergedfiles=${extended_segments}/mergedfiles
+qsmergedfiles=${mergedfiles}/qsmergedfiles
+cnvsqsfiltered=${qsmergedfiles}/cnvsqsfiltered
+
+if [ ! $(ls -A ${extended_segments}) ];then
+for file in ${unique_cnvs}/*.bed;do
+	cat $file|awk 'var=0.5*($3-$2){printf $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"8"\t"}{printf "%.0f\n", var}'| \
+	awk 'min=$2-$9 {if (min <0) min=$2;else min=min;print $1"\t"min"\t"$3+$9"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\\"}'| \
+	sed 's/chr//g'|sort  -nk1,1 -nk2,2|sed 's/^/chr/g' \
+	> ${extended_segments}/$(basename $file .bed)_ext.bed;
+done
+else
+	echo "Segment extension is already done"
+fi
+
+# Step to Merge overlapping intervals resulting from the previous step, using bedtools merge
+#conda activate pipeline
+for file in ${extended_segments}/*_ext.bed;do 
+	bedtools merge -i $file \
+	> ${mergedfiles}/$(basename $file _ext.bed)_merged.bed;
+done
+
+for file in ${mergedfiles}/*_merged.bed;do
+        bedtools intersect -a $file -b  ${newfiles}/$(basename $file _merged.bed)_type.bed -wa -wb| \
+        sort -nrk1,8|awk '!a[$2] ++'|sed 's/:/\t/g'|sed 's/chr//g'| \
+        sort -nk1,1 -nk2,2|sed 's/^/chr/g'|cut -f1,2,3,7,8,10,11,12 > ${qsmergedfiles}/$(basename $file _merged.bed)_qsmerged.bed;
+done
+
+# In this step, a dynamic QS is defined based on CN and NP for each CNV.Files are saved in cnvfiltered directory. 
+#conda activate pipeline
+
+for file in ${qsmergedfiles}/*_qsmerged.bed;do
+         awk '{if ($6==0 && $7<40) {print $0,"\t"400;} else if ($6==0 && $7>=40 && $7<100) {print $0,"\t"$7*10;} \
+         else if ($6==0 && $7>=100) {print $0,"\t"1000;} else if ($6==1 && $7<10) {print $0,"\t"100;} \
+         else if ($6==1 && $7>=10 && $7<100) {print $0,"\t"$7*10;} else if ($6==1 && $7>=100) {print $0,"\t"1000;} \
+         else if ($6 >=3 && $7<13) {print $0,"\t"50;} else if ($6>=3 && $7>=13 && $7<100) {print $0,"\t"$7*4;} \
+         else {print $0,"\t"100;}}' $file \
+         > ${cnvsqsfiltered}/$(basename $file _qsmerged.bed)_filtered.bed;
+done
+
+# In this step, each CNV dynamic QS < QS is filtered out from the sample, just keeping high-quality CNVs for further analysis.
+
+for file in ${cnvsqsfiltered}/*_filtered.bed;do 
+        awk '{if ($5> $8) print $0}' $file \
+        > ${cnvsqsfiltered}/$(basename $file _filtered.bed).bed;
+done 
+
